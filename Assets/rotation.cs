39bea9a0125c6287b827bf3eb1c6c4cd69d27ce4
Svetlana <wannasleep32@gmail.com>
2023-05-01 23:35:22 +0700

    добавление

---
+++ Assets/rotation.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+    public int x;
+    public int y;
+    public int z;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate(x, y, z);
+    }
+}
